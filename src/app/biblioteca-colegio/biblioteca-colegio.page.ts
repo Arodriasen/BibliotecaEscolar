8f3a788b45a9025d7f87bc41cfef2a35ce8c5d7b
arodriasen <arodriasen@gmail.com>
2021-05-26 20:47:12 +0200

    Página biblioteca colegio

---
+++ src/app/biblioteca-colegio/biblioteca-colegio.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-biblioteca-colegio',
+  templateUrl: './biblioteca-colegio.page.html',
+  styleUrls: ['./biblioteca-colegio.page.scss'],
+})
+export class BibliotecaColegioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
