7d5b3ae0df74fd913a578c8f0e2384fe95fbe6d7
arodriasen <arodriasen@gmail.com>
2021-05-26 17:11:37 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+
+  {
+    path: 'perfil-de-usuario',
+    loadChildren: () => import('./perfil-de-usuario/perfil-de-usuario.module').then( m => m.PerfilDeUsuarioPageModule)
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/perfil-de-usuario/perfil-de-usuario.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-de-usuario',
+  templateUrl: './perfil-de-usuario.page.html',
+  styleUrls: ['./perfil-de-usuario.page.scss'],
+})
+export class PerfilDeUsuarioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
